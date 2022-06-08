6bb98f7d557464d8cc15a26bd43eab3375a15042
Hazdan <me.manuelromero@gmail.com>
2022-06-08 11:04:41 -0500

    Say hello in spanish

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -4,13 +4,16 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
+	}
+	if language == "ES" {
+		return "Hola, " + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Manuel"))
+	fmt.Println(Hello("Manuel", ""))
 }
--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -11,12 +11,17 @@ func TestHello(t *testing.T) {
 		}
 	}
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Manuel")
+		got := Hello("Manuel", "")
 		want := "Hello, Manuel"
 		assertCorrectMessage(t, got, want)
 	})
+	t.Run("saying hello in spanish", func(t *testing.T) {
+		got := Hello("Manuel", "ES")
+		want := "Hola, Manuel"
+		assertCorrectMessage(t, got, want)
+	})
 	t.Run("say hello when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
 		assertCorrectMessage(t, got, want)
 	})
