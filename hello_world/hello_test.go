5bca72798f26b77b3103538cde73039a749f8b96
Hazdan <me.manuelromero@gmail.com>
2022-06-08 10:46:01 -0500

    Say hello when an empty string is supplied

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Manuel")
-	want := "Hello, Manuel"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Manuel")
+		want := "Hello, Manuel"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say hello when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
 }
