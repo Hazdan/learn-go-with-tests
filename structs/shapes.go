7a97098e37e0c348bd9b6e88ac61e36dd6f55e1d
Hazdan <me.manuelromero@gmail.com>
2022-06-09 10:06:10 -0500

    Structs and interfaces

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,9 +1,33 @@
 package shapes
 
-func Perimeter(width float64, height float64) float64 {
-	return (width + height) * 2
+import "math"
+
+type Shape interface {
+	Area() float64
+	Perimeter() float64
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.Radius, 2)
+}
+
+func (c Circle) Perimeter() float64 {
+	return math.Pi * c.Radius * 2
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -2,20 +2,51 @@ package shapes
 
 import "testing"
 
-func TestShape(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
-	expected := 40.0
+func TestPerimeter(t *testing.T) {
+	t.Run("perimeter for rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := rectangle.Perimeter()
+		expected := 36.0
 
-	if got != expected {
-		t.Errorf("expected %.2f but got %.2f", expected, got)
-	}
+		if got != expected {
+			t.Errorf("expected %.2f but got %.2f", expected, got)
+		}
+	})
+
+	t.Run("perimeter for circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Perimeter()
+		expected := 62.83185307179586
+
+		if got != expected {
+			t.Errorf("expected %g but got %g", expected, got)
+		}
+	})
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	expected := 100.0
 
-	if got != expected {
-		t.Errorf("expected %.2f but got %.2f", expected, got)
+	checkArea := func(t testing.TB, shape Shape, expected float64) {
+		t.Helper()
+
+		got := shape.Area()
+
+		if got != expected {
+			t.Errorf("expected %.2f but got %.2f", expected, got)
+		}
 	}
+
+	t.Run("area for rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		expected := 72.0
+
+		checkArea(t, rectangle, expected)
+	})
+
+	t.Run("area for circle", func(t *testing.T) {
+		circle := Circle{10}
+		expected := 314.1592653589793
+
+		checkArea(t, circle, expected)
+	})
 }
